a172558447f5c93c9d26880e3866ca97f717e0da
Stutuer-sn <24125267@bjtu.edu.cn>
2025-04-12 20:10:51 +0800

    feat: add CORS middleware for cross-origin request handling
    - Implemented a CORS middleware function to set headers for cross-origin requests
    - Allowed specific origin, headers, methods, and credentials
    - Handled preflight OPTIONS requests with a 204 status response

--- internal/web/init_web.go
+++ internal/web/init_web.go
@@ -2,13 +2,29 @@ package web
 
 import "github.com/gin-gonic/gin"
 
-
 // UserInitRouts 初始化路由
 func UserInitRouts(r *gin.Engine) {
 	var u *UserHandler
 	ug := r.Group("/user") // 用户相关路由
 	ug.POST("/signup", u.SignUp)
 	ug.POST("/login", u.Login)
-	ug.PUT("/edit", u.Edit) 
+	ug.PUT("/edit", u.Edit)
 	ug.GET("/profile", u.Profile)
+}
+
+// Cors 设置
+func Cors(r *gin.Engine) {
+	// 设置 CORS 跨域请求头
+	r.Use(func(c *gin.Context) {
+		// 允许的域名
+        allowedOrigin := "https://localhost:8080"
+        c.Header("Access-Control-Allow-Origin", allowedOrigin)          
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")     // 允许的请求头
+		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS") // 允许的请求方法
+		c.Header("Access-Control-Allow-Credentials", "true")                        // 允许携带凭证
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+	})
 }
--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"github.com/Fairy-nn/inspora/internal/web"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	// 创建一个新的 Gin 路由器实例
-	r := gin.Default()
-	// 初始化路由
-	web.UserInitRouts(r)
-	// 启动服务器，监听在 8080 端口
-	r.Run(":8080")
+	r := gin.Default() // 创建一个新的 Gin 路由器实例
+
+	web.Cors(r) // CORS 跨域设置
+
+	web.UserInitRouts(r) // 初始化路由
+
+	r.Run(":8080") // 启动服务器，监听在 8080 端口
 
 }
