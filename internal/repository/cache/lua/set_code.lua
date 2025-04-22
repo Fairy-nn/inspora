b4b5be234f18245a6f99362b97d6a55286f7f874
Stutuer-sn <24125267@bjtu.edu.cn>
2025-04-22 12:36:51 +0800

    feat: add Redis Lua script for verification code management
    - Added set_code.lua script to handle verification code storage
    - Implemented limits on code refresh frequency
    - Added counter for verification attempts

---
+++ internal/repository/cache/lua/set_code.lua
@@ -0,0 +1,15 @@
+--验证码在redis上的key
+local key=KEY[1] --验证码的key
+local cntKey=key..":cnt" --验证码的key+cnt
+local val=ARGV[1] --验证码的值
+local ttl=tonumber(redis.call("ttl",key))  --验证码的过期时间
+
+if ttl==-1 then --键已设置但没有过期时间
+    return -2
+elseif ttl==-2 or ttl<540 then --键不存在或过期时间小于540秒
+    redis.call("set", key, val)
+    redis.call("expire", key, 600) 
+    redis.call("set", cntKey, 3)
+    redis.call("expire", cntKey, 600)
+else --键存在且过期时间大于540秒
+    return -1    
